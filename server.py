0f3b7b184928fd941403b6de82288972445b4e17
wender103 <wendertestes01@gmail.com>
2024-01-05 11:06:58 -0300

    novo método

--- server.py
+++ server.py
@@ -1,65 +1,34 @@
-import http.server
-import socketserver
-import subprocess
-import json
-from urllib.parse import parse_qs
+import socket
+import binascii
 
-class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/open_spotify':
-            # Adicionar cabeçalhos CORS
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.send_header("Access-Control-Allow-Origin", "*")  # Isso permite qualquer origem
-            self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-            self.send_header("Access-Control-Allow-Headers", "Content-Type")
-            self.end_headers()
+def wake_on_lan(mac_address):
+    # Cria um socket UDP
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-            response_data = {"status": "success", "message": "Conexão estabelecida"}
-            self.wfile.write(json.dumps(response_data).encode())
-        else:
-            # Servir arquivos estáticos
-            super().do_GET()
+    # Endereço de broadcast e porta padrão para WoL
+    #192.168.1.255
+    #192.168.0.114
+    broadcast_address = ('192.168.1.255', 7)
 
-    def do_OPTIONS(self):
-        # Adicionar cabeçalhos CORS para lidar com solicitações OPTIONS
-        self.send_response(200)
-        self.send_header("Access-Control-Allow-Origin", "*")  # Isso permite qualquer origem
-        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        self.send_header("Access-Control-Allow-Headers", "Content-Type")
-        self.end_headers()
+    # Formata o Magic Packet usando o endereço MAC
+    mac_address = mac_address.replace('-', '').replace(':', '')
+    
+    # Adiciona um zero à frente se o comprimento da string for ímpar
+    if len(mac_address) % 2 != 0:
+        mac_address = '0' + mac_address
 
-    def do_POST(self):
-        if self.path == '/open_spotify':
-            # Obter o corpo da solicitação
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            comando = json.loads(post_data)
+    try:
+        mac_hex = binascii.unhexlify(mac_address)
+        magic_packet = b'\xff' * 6 + mac_hex * 16
 
-            # Adicionar cabeçalhos CORS
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.send_header("Access-Control-Allow-Origin", "*")  # Isso permite qualquer origem
-            self.end_headers()
+        # Envia o Magic Packet
+        s.sendto(magic_packet, broadcast_address)
+        print("Magic Packet enviado com sucesso!")
 
-            # Analisar o objeto e realizar ações correspondentes
-            if comando.get('Comando') == 'Abrir' and comando.get('Programa') == 'Spotify':
-                # Comando para abrir o Spotify no Windows (ajuste conforme necessário)
-                command = "start spotify"
-                subprocess.Popen(command, shell=True)
+    except binascii.Error:
+        print("Erro: A string contém caracteres não hexadecimais.")
 
-                response_data = {"status": "success", "message": "Comando executado"}
-                self.wfile.write(json.dumps(response_data).encode())
-            else:
-                response_data = {"status": "error", "message": "Comando não reconhecido"}
-                self.wfile.write(json.dumps(response_data).encode())
-        else:
-            # Servir arquivos estáticos
-            super().do_GET()
-
-PORT = 8000
-Handler = MyRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Servindo na porta {PORT}")
-    httpd.serve_forever()
+# Endereço MAC do seu PC
+#AC-22-0B-2E-13-5C
+wake_on_lan('AC-22-0B-2E-13-5C')
