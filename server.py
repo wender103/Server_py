717548a16927a502479a3eafaad7ff8a163d057b
wender103 <wendertestes01@gmail.com>
2024-01-05 12:35:02 -0300

    ...

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
+from flask_sslify import SSLify
 import socket
 import binascii
-from flask_sslify import SSLify
 
 app = Flask(__name__)
 sslify = SSLify(app)
@@ -13,8 +12,6 @@ def wake_on_lan(mac_address):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
     # Endereço de broadcast e porta padrão para WoL
-    #192.168.1.255
-    #192.168.0.114
     broadcast_address = ('192.168.1.255', 7)
 
     # Formata o Magic Packet usando o endereço MAC
@@ -35,15 +32,18 @@ def wake_on_lan(mac_address):
     except binascii.Error:
         print("Erro: A string contém caracteres não hexadecimais.")
 
-# Endereço MAC do seu PC
-#AC-22-0B-2E-13-5C
-# wake_on_lan('AC-22-0B-2E-13-5C')
-
 @app.route('/wake-on-lan', methods=['POST'])
 def wake_on_lan_endpoint():
     mac_address = request.json.get('mac_address')
     wake_on_lan(mac_address)
-    return 'Magic Packet enviado com sucesso!'
+
+    # Configurar cabeçalhos CORS
+    response = jsonify({'message': 'Magic Packet enviado com sucesso!'})
+    response.headers.add('Access-Control-Allow-Origin', 'https://wender103.github.io')
+    response.headers.add('Access-Control-Allow-Methods', 'POST')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
